ef051dae07aafba6767624044fe36ed14c8941a8
Luciano Closs <lcloss@users.noreply.github.com>
2019-08-18 15:53:26 +0100

    Initial commit

---
+++ DatabaseStats.sql
@@ -0,0 +1,16 @@
+SELECT t.name AS TableName
+     , i.name AS IndexName
+	 , p.[Rows]
+	 , SUM(a.total_pages) AS TotalPages
+	 , SUM(a.used_pages) AS UsedPages
+	 , SUM(a.data_pages) AS DataPages
+	 , ( SUM(a.total_pages) * 8) / 1024 as TotalSpaceMB
+	 , ( SUM(a.used_pages) * 8) / 1024 as UsedSpaceMB
+	 , ( SUM(a.data_pages) * 8) / 1024 as DataSpaceMB
+  FROM sys.tables t
+  INNER JOIN sys.indexes i ON t.object_id = i.object_id
+  INNER JOIN sys.partitions p ON i.object_id = p.object_id AND i.index_id = p.index_id
+  INNER JOIN sys.allocation_units a ON p.partition_id = a.container_id
+  WHERE p.[Rows] > 200000
+  GROUP BY t.name, i.object_id, i.index_id, i.name, p.[Rows]
+  ORDER BY OBJECT_NAME(i.object_id)
